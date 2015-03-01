efdfe3f0ee34d9ae90939d8d75935d4562b1d94d
John French <jdaviesf7@gmail.com>
2015-03-01 06:17:02 -0500

    Added pathogen and solarized for vim, and improved mklinks

--- mklinks.zsh
+++ mklinks.zsh
@@ -2,12 +2,17 @@
 setopt EXTENDED_GLOB
 
 # set up zsh dotfiles from prezto
-ln -s $0:A:h/zprezto "${HOME}/.zprezto"
+ln -sfn $0:A:h/zprezto "${HOME}/.zprezto"
 for rcfile in "${HOME}"/.zprezto/runcoms/^README.md(.N); do
-	ln -s "$rcfile" "${HOME}/.${rcfile:t}"
+	ln -sfn "$rcfile" "${HOME}/.${rcfile:t}"
 done
+
+# install vim pathogen
+mkdir -p "${HOME}/.vim"
+ln -sfn $0:A:h/vim-pathogen/autoload "${HOME}/.vim/autoload"
+ln -sfn $0:A:h/vim-bundle "${HOME}/.vim/bundle"
 
 # set up other dotfiles
 for rcfile in $0:A:h/^$0:t(.); do
-	ln -s "$rcfile" "${HOME}/.${rcfile:t}"
+	ln -sfn "$rcfile" "${HOME}/.${rcfile:t}"
 done
